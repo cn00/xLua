a6b20191e2c84950272994dcad5f45cfc3df12b8
cn <cool_navy@qq.com>
2020-02-22 02:30:38 +0800

    add lsqlite3

--- build/make_osx_lua53.sh
+++ build/make_osx_lua53.sh
@@ -7,6 +7,3 @@ mkdir -p $thisdir/build_osx && pushd $_
 cmake -GXcode ../
 popd
 cmake --build build_osx --config Release
-mkdir -p plugin_lua53/Plugins/xlua.bundle/Contents/MacOS/
-cp -r build_osx/Release/xlua.bundle ../Assets/Plugins
-
