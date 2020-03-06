07e8622f327046ca277c324c78295be5ad2c014b
cn <cool_navy@qq.com>
2020-03-07 00:29:01 +0800

    android ios ok

--- build/make_android_lua53.sh
+++ build/make_android_lua53.sh
@@ -13,6 +13,8 @@ if [ ! -d "$NDK" ]; then
     exit 1
 fi
 
+cd $(dirname $0)
+
 function build() {
     API=$1
     ABI=$2
@@ -21,9 +23,14 @@ function build() {
     cmake -H. -B${BUILD_PATH} -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN=clang -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_ANME}
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p plugin_lua53/Plugins/Android/libs/${ABI}/
-    cp ${BUILD_PATH}/libxlua.so plugin_lua53/Plugins/Android/libs/${ABI}/libxlua.so
+    # cp ${BUILD_PATH}/libxlua.so ../../Assets/XLua/Plugins/Android/libs/${ABI}/libxlua.so
 }
 
 build android-16 armeabi-v7a arm-linux-androideabi-4.9
+cp build.Android.armeabi-v7a/*.so ../../Assets/XLua/Plugins/Android/libs/armeabi-v7a/
+
 build android-16 arm64-v8a  arm-linux-androideabi-clang
+cp build.Android.arm64-v8a/*.so ../../Assets/XLua/Plugins/Android/libs/arm64-v8a/
+
 build android-16 x86 x86-4.9
+cp build.Android.x86/*.so ../../Assets/XLua/Plugins/Android/libs/x86/
