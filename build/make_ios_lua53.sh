c9e5b4da11b79143fd3fb9d233523f622fb3859a
cn <cool_navy@qq.com>
2020-03-16 21:44:45 +0800

    rm cp

--- build/make_ios_lua53.sh
+++ build/make_ios_lua53.sh
@@ -6,6 +6,4 @@ cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -GXcode ../
 cd ..
 cmake --build build_ios --config Release
 # cmake --build build_ios --config Release
-mkdir -p plugin_lua53/Plugins/iOS/
-cp build_ios/Release-iphoneos/libxlua.a plugin_lua53/Plugins/iOS/libxlua.a 
 
