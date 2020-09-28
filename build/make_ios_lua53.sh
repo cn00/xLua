d85bd79b83594efd0cd734b80d4296e17a7b67d5
cn <cool_navy@qq.com>
2020-09-28 10:12:42 +0800

    add PLATFORM_INT=OS64COMBINED for simulator

--- build/make_ios_lua53.sh
+++ build/make_ios_lua53.sh
@@ -2,7 +2,7 @@ cd $(dirname $0;pwd)
 mkdir -p build_ios && cd build_ios
 # cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
 # cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS.cmake -GXcode ../
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -GXcode ../
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM_INT="OS64COMBINED" -GXcode ../
 cd ..
 cmake --build build_ios --config Release
 # cmake --build build_ios --config Release
