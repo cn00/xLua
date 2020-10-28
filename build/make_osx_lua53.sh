0135f7c095c1d3fbc760465356a8a36da8832f5e
cn <cool_navy@qq.com>
2020-10-28 20:25:18 +0800

    up build reference

--- build/make_osx_lua53.sh
+++ build/make_osx_lua53.sh
@@ -7,6 +7,8 @@ mkdir -p $thisdir/build_osx && pushd $_
 cmake --clean-first -GXcode ../
 popd
 cmake --build build_osx --config Release
+# cmake --build build_osx --config Debug
 
 cd build_osx
 cp -Lvf bin/Release/*.dylib ../../../bin/
+# cp -Lvf bin/Debug/*.dylib ../../../bin/
