598139b43a39f151dc7084cb7f1ca0ad609e2e9a
cn <cool_navy@qq.com>
2020-11-02 22:20:41 +0800

    up nslua

--- build/make_android_lua53.sh
+++ build/make_android_lua53.sh
@@ -19,19 +19,20 @@ androidapi='android-21'
 
 function build() {
     API=${androidapi}
-    ABI=$1
+    export ABI=$1
+    export ANDROID_ABI=${ABI}
     TOOLCHAIN_NAME=$2
     BUILD_PATH=build.Android.${ABI}
     cmake -H. -B${BUILD_PATH} -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN=clang -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_ANME} -DANDROID_ABI=${ABI}
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p ../../Assets/XLua/Plugins/Android/libs/${ABI}/
-    cp -v ${BUILD_PATH}/libs/*.so ../../Assets/XLua/Plugins/Android/libs/${ABI}/
+    cp -v ${BUILD_PATH}/bin/*.so ../../Assets/XLua/Plugins/Android/libs/${ABI}/
 }
 
 build armeabi-v7a arm-linux-androideabi-4.9
 
-build arm64-v8a  arm-linux-androideabi-clang
+# build arm64-v8a  arm-linux-androideabi-clang
 
-build x86 x86-4.9
+# build x86 x86-4.9
 
-build x86_64 x86_64-4.9
+# build x86_64 x86_64-4.9
