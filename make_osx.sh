aad0ceeeadd1ce3d0e5e3ce9a2fe4097570408ff
cn <cool_navy@qq.com>
2020-12-09 12:15:48 +0800

    update xlua cmake build

---
+++ make_osx.sh
@@ -0,0 +1,33 @@
+
+
+thisdir=$(cd `dirname $0`;pwd)
+cd "$thisdir"
+
+build_type="Release" #Debug
+mkdir -p $thisdir/build.OSX && pushd $_
+cmake --clean-first -GXcode ../build
+popd
+cmake --build build.OSX --config ${build_type}
+
+# cmake --install build.OSX --prefix ../../cslua/bin/Debug/
+# cmake --install build.OSX --prefix ../../cslua/bin/Release/
+
+for i in `ls build.OSX/bin/${build_type}/`; do 
+    install_name_tool -id @loader_path/$i build.OSX/bin/${build_type}/$i;
+    install_name_tool -change @executable_path/../lib/liblua.dylib @loader_path/liblua.dylib build.OSX/bin/${build_type}/$i;
+done
+
+## fixed Killed: 9
+/usr/bin/codesign --force --sign - --timestamp=none build.OSX/bin/${build_type}/*.dylib
+
+outdir='Assets/XLua/Plugins/OSX/'
+mkdir -p ${outdir}
+cp -vf build.OSX/bin/${build_type}/* ${outdir}
+
+outdir='./lib/'
+mkdir $outdir
+# cp -vf build.OSX/bin/${build_type}/*.dylib ../lib/
+cp -vf build.OSX/bin/${build_type}/* ${outdir}
+# rm -rf build.OSX/bin/${build_type}.bak
+# mv -f build.OSX/bin/${build_type} build.OSX/bin/${build_type}.bak
+
